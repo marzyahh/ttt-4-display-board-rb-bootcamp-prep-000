5572056d6fcf58c9f8bf68082a3df9135b8c0f2c
marzyah <marzyahhooseini74@gmail.com>
2020-05-30 06:42:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -24,12 +24,12 @@ def display_board(board)
 end
 
 
-board = ["", "", "", "", "X", "", "", "", ""]
+board = ["O", "", "", "", "", "", "", "", ""]
 def display_board(board)
 
-  puts  "   |   |   "
+  puts  " #{board[0]}  |   |   "
   puts  "-----------"
-  puts  "   | #{board[4]} |   "
+  puts  "   |   |   "
   puts  "-----------"
   puts  "   |   |   "
 
