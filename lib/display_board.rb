d771400f241f3612230c300516c46e9a25800ae9
marzyah <marzyahhooseini74@gmail.com>
2020-05-30 06:47:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -17,7 +17,7 @@ def display_board(board)
 
   puts  " #{board[0]} | #{board[1]} | #{board[2]} "
   puts  "-----------"
-  puts  "   | #{board[4]} |   "
+  puts  " #{board[3]} | #{board[4]} | #{board[5]}  "
   puts  "-----------"
   puts  "   |   | #{board[8]} "
 
